d822ca994f841c26ab8d29b2514767257e7c7f43
Edwin Hernandez <edwin.hernandez@garonz.com>
2017-10-02 23:10:01 -0400

    Registro y autenticacion con jwt

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/name', function(Request $request){
+	$token = JWTAuth::getToken();
+
+	$user = JWTAuth::toUser($token);
+
+	return $user;
+})->middleware('jwt.auth');
+
+Route::post('/authenticate', [
+	'uses' => 'ApiAuthController@authenticate']);
+
+Route::post('/register',[
+	'uses' => 'ApiAuthController@register']);
